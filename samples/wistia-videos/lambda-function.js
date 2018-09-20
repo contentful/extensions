6907393069cda2c2deac8877b160c062d90b3cb4
Jeff Reiner <theundeadghost@gmail.com>
2018-09-20 23:12:51 +0200

    :sparkles: Finishing touches on the wistia-videos extension

--- samples/wistia-videos/lambda-function.js
+++ samples/wistia-videos/lambda-function.js
@@ -12,14 +12,43 @@ const request = ({ method, hostname, path, headers }) =>
       .end();
   });
 
+const cleanup = res =>
+  res.map(video => {
+    [
+      "embedCode",
+      "progress",
+      "status",
+      "assets",
+      "type",
+      "created",
+      "updated"
+    ].forEach(e => delete video[e]);
+
+    return {
+      ...video,
+      thumbnail: video.thumbnail.url.replace(/\?image_crop_resized.*/, "")
+    };
+  });
+
 exports.handler = function(event, context, callback) {
   request({
     method: "GET",
     hostname: `api.wistia.com`,
     path: `/v1/medias.json?api_password=${process.env.WISTIA_API_KEY}`
   })
-    .then(res => callback(res))
+    .then(cleanup)
+    .then(res =>
+      callback(null, {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(res)
+      })
+    )
     .catch(err => {
+      console.log(err);
       callback(err);
     });
 };
