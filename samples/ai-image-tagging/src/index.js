c55ccdc4905a73eb0023de8a986542f5d8d50c79
Martin Lehner <lehner.m.e@gmail.com>
2019-03-26 10:46:27 +0100

    Extract image tagging into separate component

--- samples/ai-image-tagging/src/index.js
+++ samples/ai-image-tagging/src/index.js
@@ -1,93 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import { Button, CheckboxField } from '@contentful/forma-36-react-components';
 import { init, locations } from 'contentful-ui-extensions-sdk';
 
 import { ImageTaggingHelp } from './components/ImageTaggingHelp'
 import { ImageTaggingLocationError } from './components/ImageTaggingLocationError';
 import { isCompatibleTagField, isCompatibleImageField, getField } from './lib/content-type';
-import { mergeTags, requestTags } from './lib/tags';
 
 import '@contentful/forma-36-react-components/dist/styles.css';
 import '@contentful/forma-36-fcss'
 import './index.css';
+import { ImageTagging } from './components/ImageTagging';
 
 class App extends React.Component {
   static propTypes = {
     sdk: PropTypes.object.isRequired,
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      overwriteTags: true,
-    }
-  }
-
   componentDidMount() {
     this.props.sdk.window.startAutoResizer();
   }
 
-  loadTags = async () => {
-    const bail = (message) => {
-      notifier.error(message);
-      this.setState({
-        loadingTags: false,
-      })
-    };
-
-    this.setState({
-      loadingTags: true,
-    });
-
-    const { sdk } = this.props;
-    const { entry, space, notifier, parameters: { instance: { tagFieldId, imageFieldId } } } = sdk;
-
-    const { overwriteTags } = this.state;
-    const defaultLocale = sdk.locales.default;
-
-    let imageUrl;
-    try {
-      const assetId = entry.fields[imageFieldId].getValue().sys.id;
-      const asset = await space.getAsset(assetId);
-      imageUrl = asset.fields.file[defaultLocale].url;
-
-      if (!imageUrl) {
-        bail('Add an image to the entry first');
-        return
-      }
-    } catch (e) {
-      bail('Add an image to the entry first');
-      return
-    }
-
-    try {
-      const newTags = await requestTags('https:' + imageUrl + '?w=1080');
-
-      const tags = overwriteTags ? newTags : mergeTags(entry.fields[tagFieldId].getValue() || [], newTags);
-      entry.fields[tagFieldId].setValue(tags);
-      notifier.success('Tags added!')
-    } catch (e) {
-      bail('Failed to load tags for image')
-    }
-
-    this.setState({
-      loadingTags: false,
-    })
-  };
-
-  onTagMergeChanged = (event) => {
-    const overwriteTags = event.target.checked;
-
-    this.setState({
-      overwriteTags,
-    })
-  };
-
   render() {
     const { contentType, field, location, parameters: { instance: { imageFieldId, tagFieldId } } } = this.props.sdk;
-    const { loadingTags, overwriteTags } = (this.state || {});
 
     const hasImageField = isCompatibleImageField(getField(contentType, imageFieldId));
     const hasTagField = isCompatibleTagField(getField(contentType, tagFieldId));
@@ -105,28 +40,7 @@ class App extends React.Component {
       />
     }
 
-    return (
-      <div className='f36-color--text-light'>
-        <CheckboxField
-          labelText='Overwrite existing tags'
-          name='overwriteTags'
-          checked={overwriteTags}
-          labelIsLight={true}
-          onChange={this.onTagMergeChanged}
-          id='overwriteTags'
-          extraClassNames='config_overwrite_tags'
-        />
-        <Button
-          buttonType='muted'
-          isFullWidth={true}
-          onClick={this.loadTags}
-          loading={loadingTags}
-          disabled={loadingTags}
-        >
-          Auto-tag image
-        </Button>
-      </div>
-    );
+    return <ImageTagging sdk={this.props.sdk}/>
   };
 }
 
