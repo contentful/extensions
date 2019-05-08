ea73532e8e3ef76eca72f4a77edfc0a8cf37023c
Jakub Elżbieciak <jakub@contentful.com>
2019-05-08 16:58:09 +0200

    Optimizely app scaffolding

---
+++ samples/optimizely/src/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+import { init, locations } from './sdk';
+
+import '@contentful/forma-36-react-components/dist/styles.css';
+
+import App from './app';
+
+init((sdk) => {
+  if (sdk.location.is(locations.LOCATION_ENTRY_EDITOR)) {
+    render(<App sdk={sdk} />, document.getElementById('root'));
+  }
+});
