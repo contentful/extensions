899497677a28a65f3461c8fc228c3e87d2536b59
Alex Suevalov <alex.suevalov@contentful.com>
2019-05-14 15:55:02 +0200

    Show description when experiment is selected

--- samples/optimizely/src/components/experiment-section.js
+++ samples/optimizely/src/components/experiment-section.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { css } from 'emotion';
-import { Heading, SelectField, Option } from '@contentful/forma-36-react-components';
+import { Heading, SelectField, Option, Paragraph } from '@contentful/forma-36-react-components';
 import tokens from '@contentful/forma-36-tokens';
 
 const styles = {
   heading: css({
     marginBottom: tokens.spacingL
+  }),
+  description: css({
+    marginTop: tokens.spacingS,
+    color: tokens.colorTextLight
   })
 };
 
 export default function ExperimentSection(props) {
+  const experiment = useMemo(() => {
+    return props.experiments.find(experiment => experiment.id.toString() === props.experimentId);
+  }, [props.experimentId, props.experiments]);
+
   return (
     <React.Fragment>
       <Heading element="h2" className={styles.heading}>
@@ -19,25 +27,35 @@ export default function ExperimentSection(props) {
       <SelectField
         labelText="Optimizely experiment"
         required
-        value=""
-        onChange={() => {}}
+        value={props.experimentId}
+        onChange={e => {
+          props.onChangeExperiment(e.target.value);
+        }}
         selectProps={{ width: 'large', isDisabled: props.loaded === false }}
         id="experiment"
         name="experiment">
         {props.loaded === false && <Option value="-1">Fetching experiments...</Option>}
-        {props.loaded &&
-          props.experiments.map(experiment => (
-            <Option key={experiment.id} value={experiment.id}>
-              {experiment.name || experiment.key} ({experiment.status})
-            </Option>
-          ))}
+        {props.loaded && (
+          <React.Fragment>
+            <Option value="-1">Select Optimizely experiment</Option>
+            {props.experiments.map(experiment => (
+              <Option key={experiment.id.toString()} value={experiment.id.toString()}>
+                {experiment.name || experiment.key} ({experiment.status})
+              </Option>
+            ))}
+          </React.Fragment>
+        )}
       </SelectField>
+      {experiment && experiment.description && (
+        <Paragraph className={styles.description}>Description: {experiment.description}</Paragraph>
+      )}
     </React.Fragment>
   );
 }
 
 ExperimentSection.propTypes = {
   loaded: PropTypes.bool.isRequired,
+  experimentId: PropTypes.string,
   experiments: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
@@ -46,7 +64,8 @@ ExperimentSection.propTypes = {
       description: PropTypes.string,
       status: PropTypes.string.isRequired
     }).isRequired
-  ).isRequired
+  ).isRequired,
+  onChangeExperiment: PropTypes.func.isRequired
 };
 
 ExperimentSection.defaultProps = {
