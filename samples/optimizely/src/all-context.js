50ecb1e8e53ef21cc8881d9904eabada9a92be4e
Alex Suevalov <alex.suevalov@contentful.com>
2019-05-15 18:21:48 +0200

    Create new variations is ready

--- samples/optimizely/src/all-context.js
+++ samples/optimizely/src/all-context.js
@@ -3,3 +3,5 @@ import React from 'react';
 export const SDKContext = React.createContext(null);
 
 export const ContentTypesContext = React.createContext([]);
+
+export const ReferenceInfoContext = React.createContext(null);
