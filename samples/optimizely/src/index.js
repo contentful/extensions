7b86336865bf464d7d3f69cdfc66b907e39f7925
Alex Suevalov <alex.suevalov@contentful.com>
2019-05-20 11:50:49 +0200

    Validate content type before rendering an extension

--- samples/optimizely/src/index.js
+++ samples/optimizely/src/index.js
@@ -4,16 +4,25 @@ import { init, locations } from './sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.css';
 import OptimizelyClient from './optimizely-client';
-
 import App from './app';
+import IncorrectContentType, { isValidContentType } from './components/incorrect-content-type';
+
+function renderExtension(elem) {
+  render(elem, document.getElementById('root'));
+}
 
 init(sdk => {
   if (sdk.location.is(locations.LOCATION_ENTRY_EDITOR)) {
-    const client = new OptimizelyClient({
-      sdk: sdk,
-      project: sdk.parameters.installation.optimizelyProjectId
-    });
-    render(<App sdk={sdk} client={client} />, document.getElementById('root'));
+    const [valid, missingFields] = isValidContentType(sdk.contentType);
+    if (valid) {
+      const client = new OptimizelyClient({
+        sdk: sdk,
+        project: sdk.parameters.installation.optimizelyProjectId
+      });
+      renderExtension(<App sdk={sdk} client={client} />);
+    } else {
+      renderExtension(<IncorrectContentType sdk={sdk} missingFields={missingFields} />);
+    }
   }
 });
 
