d17da61421bf706079da0e8abe759a52b781e70d
Alex Suevalov <alex.suevalov@contentful.com>
2019-06-27 10:41:00 +0200

    Fix prettier

--- marketplace/mux-video-uploader/jest.config.js
+++ marketplace/mux-video-uploader/jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   setupFiles: ['<rootDir>/test/testSetup.ts'],
   moduleNameMapper: {
-    '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules'
-  }
+    '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules',
+  },
 };
