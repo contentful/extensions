97e863709952f484c16e999d00f6df91a9c5f98f
David Fateh <davidfateh@gmail.com>
2019-07-03 15:45:06 +0200

    fixing message bug and changing gif for example usage

--- marketplace/netlify-build/src/contstants.js
+++ marketplace/netlify-build/src/contstants.js
@@ -3,9 +3,9 @@ export const EVENT_TRIGGER_FAILED = 'trigger-failed';
 
 export const CONTENTFUL_EVENTS = [EVENT_TRIGGERED, EVENT_TRIGGER_FAILED];
 
-const EVENT_BUILD_STARTED = 'build-started';
-const EVENT_BUILD_READY = 'build-ready';
-const EVENT_BUILD_FAILED = 'build-failed';
+export const EVENT_BUILD_STARTED = 'build-started';
+export const EVENT_BUILD_READY = 'build-ready';
+export const EVENT_BUILD_FAILED = 'build-failed';
 
 export const NETLIFY_EVENTS = [EVENT_BUILD_STARTED, EVENT_BUILD_READY, EVENT_BUILD_FAILED];
 
