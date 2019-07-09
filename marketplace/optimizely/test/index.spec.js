faffde216696713534b1472f2d39881dbfe43860
David Fateh <davidfateh@gmail.com>
2019-07-09 09:30:44 +0200

    adding test case outlines

--- marketplace/optimizely/test/index.spec.js
+++ marketplace/optimizely/test/index.spec.js
@@ -1,1 +1,27 @@
-describe('Optimizely UIE', () => {});
+import { locations } from '../src/sdk';
+
+describe('Optimizely UIE entry', () => {
+  const mockInit = jest.fn();
+
+  jest.mock('../src/sdk', () => {
+    return {
+      __esModule: true,
+      init: mockInit,
+      locations
+    };
+  });
+
+  jest.mock('../src/optimizely-client', () => {
+    return {
+      __esModule: true,
+      init: mockInit,
+      locations
+    };
+  });
+
+  it('should instantiate the optimizely client on init', () => {});
+  it('should render MissingProjectId screen if no projectId exists', () => {});
+  it('should render App if contentType is valid', () => {});
+  it('should render IncorrectContentType if contentType is invalid', () => {});
+  it('should render AppSidebar if in sidebar location', () => {});
+});
