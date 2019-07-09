e9fc2d24f0d47478d562880348ebf62d4d17a022
David Fateh <davidfateh@gmail.com>
2019-07-09 12:12:38 +0200

    starting testing

--- marketplace/optimizely/test/index.spec.js
+++ marketplace/optimizely/test/index.spec.js
@@ -1,26 +1,70 @@
-import { locations } from '../src/sdk';
+/* global global */
+import React from 'react';
+import OptimizelyClient from '../src/optimizely-client';
+import { render } from 'react-dom';
+import { MissingProjectId } from '../src/components/errors-messages';
+
+let LOCATION = '';
+const mockGetElementById = jest.fn(id => id);
+
+global.window.document.getElementById = mockGetElementById;
+
+function mockSdk() {
+  jest.doMock('../src/sdk', () => {
+    return {
+      __esModule: true,
+      init: jest.fn(fn =>
+        fn({
+          parameters: {
+            installation: {
+              optimizelyProjectId: ''
+            }
+          },
+          location: {
+            is: jest.fn(l => {
+              return l === LOCATION;
+            })
+          }
+        })
+      ),
+      locations: {
+        LOCATION_ENTRY_FIELD: 'entry-field',
+        LOCATION_ENTRY_FIELD_SIDEBAR: 'entry-field-sidebar',
+        LOCATION_ENTRY_SIDEBAR: 'entry-sidebar',
+        LOCATION_DIALOG: 'dialog',
+        LOCATION_ENTRY_EDITOR: 'entry-editor'
+      }
+    };
+  });
+
+  require('../src/sdk');
+}
+
+jest.mock('../src/optimizely-client');
+
+jest.mock('react-dom');
 
 describe('Optimizely UIE entry', () => {
-  const mockInit = jest.fn();
-
-  jest.mock('../src/sdk', () => {
-    return {
-      __esModule: true,
-      init: mockInit,
-      locations
-    };
+  beforeEach(() => {
+    mockSdk();
+    jest.isolateModules(() => {
+      require('../src/index');
+    });
   });
 
-  jest.mock('../src/optimizely-client', () => {
-    return {
-      __esModule: true,
-      init: mockInit,
-      locations
-    };
+  afterEach(() => {
+    OptimizelyClient.mockClear();
+    jest.unmock('../src/sdk');
   });
 
-  it('should instantiate the optimizely client on init', () => {});
-  it('should render MissingProjectId screen if no projectId exists', () => {});
+  it('should instantiate the optimizely client on init', () => {
+    expect(OptimizelyClient).toHaveBeenCalledTimes(1);
+  });
+  it('should render MissingProjectId screen if no projectId exists', () => {
+    LOCATION = 'entry-editor';
+    require('../src/index');
+    expect(render).toHaveBeenCalledWith(<MissingProjectId />, 'root');
+  });
   it('should render App if contentType is valid', () => {});
   it('should render IncorrectContentType if contentType is invalid', () => {});
   it('should render AppSidebar if in sidebar location', () => {});
