a170d4f3db3bfcf67ccb66333bf9c4869dc9b339
David Fateh <davidfateh@users.noreply.github.com>
2019-08-20 15:06:13 +0200

    Netlify beautify (#183)

--- marketplace/netlify-build/src/app/netlify-connection.js
+++ marketplace/netlify-build/src/app/netlify-connection.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { css } from 'emotion';
 
-import { Typography, Heading, Paragraph, Button } from '@contentful/forma-36-react-components';
+import tokens from '@contentful/forma-36-tokens';
+import { Typography, Heading, Paragraph } from '@contentful/forma-36-react-components';
+
+const styles = {
+  auth: css({
+    display: 'flex',
+    justifyContent: 'center'
+  }),
+  button: css({
+    backgroundColor: '#00ad9e',
+    color: '#fff',
+    padding: '12px 80px',
+    outline: 'none',
+    borderRadius: '6px',
+    border: '1px solid #e9ebeb',
+    borderBottom: '1px solid #e1e2e4',
+    boxShadow: '0 2px 4px 0 rgba(14,30,37,.12)',
+    cursor: 'pointer',
+    fontSize: '16px',
+    boxSizing: 'border-box'
+  }),
+  splitter: css({
+    marginTop: tokens.spacingL,
+    marginBottom: tokens.spacingL,
+    border: 0,
+    height: '1px',
+    backgroundColor: tokens.colorElementMid
+  })
+};
 
 export default class NetlifyConnection extends React.Component {
   static propTypes = {
@@ -17,7 +46,7 @@ export default class NetlifyConnection extends React.Component {
   render() {
     return (
       <Typography>
-        <Heading>Netlify account</Heading>
+        <Heading>Connect Netlify</Heading>
         {this.props.connected ? this.renderConnectionInfo() : this.renderConnectButton()}
       </Typography>
     );
@@ -31,13 +60,22 @@ export default class NetlifyConnection extends React.Component {
           Web App.
         </Paragraph>
         <Paragraph>
-          <Button buttonType="primary" onClick={this.props.onConnectClick}>
-            Connect account
-          </Button>
+          <div className={styles.auth}>
+            <button
+              buttonType="primary"
+              onClick={this.props.onConnectClick}
+              className={styles.button}>
+              Connect account
+            </button>
+          </div>
         </Paragraph>
       </>
     );
   }
+
+  getSitePlural = count => {
+    return count === 1 ? 'site' : 'sites';
+  };
 
   renderConnectionInfo() {
     const { unavailable, buildable } = this.props.netlifyCounts;
@@ -49,24 +87,29 @@ export default class NetlifyConnection extends React.Component {
         </Paragraph>
         {unavailable > 0 && (
           <Paragraph>
-            There are {unavailable} sites we can’t build because they are not configured to use
-            continuous deployment.{' '}
+            There are {unavailable} {this.getSitePlural(unavailable)} we can’t build because they
+            are not configured to use continuous deployment.{' '}
             <a href="https://app.netlify.com/" target="_blank" rel="noopener noreferrer">
               View more on Netlify
             </a>
             .
           </Paragraph>
         )}
-        {buildable > 0 && <Paragraph>{buildable} sites can be built.</Paragraph>}
+        {buildable > 0 && (
+          <Paragraph>
+            {buildable} {this.getSitePlural(buildable)} can be built.
+          </Paragraph>
+        )}
         {buildable < 1 && (
           <Paragraph>
-            There are no sites that can be built. Navigate to{' '}
+            You don&rsquo;t have any sites set up to be built on your account. Head over to{' '}
             <a href="https://app.netlify.com/" target="_blank" rel="noopener noreferrer">
               Netlify
             </a>{' '}
             to create one!
           </Paragraph>
         )}
+        <hr className={styles.splitter} />
       </>
     );
   }
