e8f4a153826f85ef225ac51f583713d5fbc09a09
Rouven Weßling <me@rouvenwessling.de>
2019-09-03 10:07:49 +0200

    Add readable extension

---
+++ marketplace/readable/src/index.spec.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render, fireEvent, cleanup, configure } from '@testing-library/react';
+import { DialogExtension, SidebarExtension } from './index';
+
+configure({
+  testIdAttribute: 'data-test-id'
+});
+
+describe('DialogExtension', () => {
+  afterEach(cleanup);
+
+  it('renders button', () => {
+    const sdk = {
+      close: jest.fn()
+    };
+    const { getByTestId } = render(<DialogExtension sdk={sdk} />);
+
+    fireEvent.click(getByTestId('close-dialog'));
+    expect(sdk.close).toHaveBeenCalledWith('data from modal dialog');
+  });
+});
+
+describe('SidebarExtension', () => {
+  afterEach(cleanup);
+
+  it('render button', () => {
+    const sdk = {
+      window: {
+        startAutoResizer: jest.fn()
+      },
+      dialogs: {
+        openExtension: jest.fn()
+      }
+    };
+    const { getByTestId } = render(<SidebarExtension sdk={sdk} />);
+
+    expect(sdk.window.startAutoResizer).toHaveBeenCalled();
+
+    fireEvent.click(getByTestId('open-dialog'));
+
+    expect(sdk.dialogs.openExtension).toHaveBeenCalledWith({
+      title: 'The same extension rendered in modal window',
+      width: 800
+    });
+  });
+});
