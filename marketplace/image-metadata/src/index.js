c9b8185fef14e80beefc5e820e387e86146fda47
Yiotis Kaltsikis <giotisgr@gmail.com>
2019-09-13 10:38:46 +0200

    Render the dialog and field extension views

--- marketplace/image-metadata/src/index.js
+++ marketplace/image-metadata/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
-import { TextInput } from '@contentful/forma-36-react-components';
-import { init } from 'contentful-ui-extensions-sdk';
+import { init, locations } from 'contentful-ui-extensions-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.css';
+
+import { FocalPointView } from './components/FocalPointView';
+import { FocalPointDialog } from './components/FocalPointDialog';
 
 export class App extends React.Component {
   static propTypes = {
@@ -16,7 +18,9 @@ export class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: props.sdk.field.getValue() || ''
+      value: props.sdk.field.getValue() || {
+        focalPoint: {}
+      }
     };
   }
 
@@ -47,28 +51,90 @@ export class App extends React.Component {
     }
   };
 
+  findProperLocale() {
+    if (this.props.sdk.entry.fields[this.props.sdk.field.id].type === 'Link') {
+      return this.props.sdk.locales.default;
+    }
+
+    return this.props.sdk.field.locale;
+  }
+
+  setFocalPoint = focalPoint => {
+    this.setState({
+      value: {
+        ...this.state.value,
+        focalPoint
+      }
+    });
+    this.props.sdk.field.setValue({
+      ...this.state.value,
+      focalPoint
+    });
+  };
+
+  showFocalPointDialog = async () => {
+    const {
+      sdk: { space, entry }
+    } = this.props;
+
+    const asset = await space.getAsset(entry.fields.articleImg.getValue().sys.id);
+    const file = asset.fields.file[this.findProperLocale()];
+
+    const focalPoint = await this.props.sdk.dialogs.openExtension({
+      id: 'image-metadata',
+      width: 1000,
+      parameters: {
+        file,
+        focalPoint: this.state.value.focalPoint
+      }
+    });
+
+    if (focalPoint) {
+      this.setFocalPoint(focalPoint);
+    }
+  };
+
   render() {
+    const { x, y } = this.state.value.focalPoint;
     return (
-      <TextInput
-        width="large"
-        type="text"
-        id="my-field"
-        testId="my-field"
-        value={this.state.value}
-        onChange={this.onChange}
-      />
+      <FocalPointView showFocalPointDialog={this.showFocalPointDialog} focalPoint={{ x, y }} />
     );
   }
 }
 
+function renderDialog(sdk) {
+  const { invocation: otherProps } = sdk.parameters;
+  const container = document.createElement('div');
+  const CONTAINER_ID = 'focal-point-dialog';
+
+  container.id = CONTAINER_ID;
+  document.body.appendChild(container);
+
+  sdk.window.startAutoResizer();
+
+  ReactDOM.render(
+    <FocalPointDialog
+      sdk={sdk}
+      onSave={data => sdk.close(data)}
+      onClose={() => sdk.close()}
+      {...otherProps}
+    />,
+    document.getElementById(CONTAINER_ID)
+  );
+}
+
 init(sdk => {
-  ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
+  if (sdk.location.is(locations.LOCATION_DIALOG)) {
+    renderDialog(sdk);
+  } else {
+    ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
+  }
 });
 
 /**
  * By default, iframe of the extension is fully reloaded on every save of a source file.
  * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
  */
-// if (module.hot) {
-//   module.hot.accept();
-// }
+if (module.hot) {
+  module.hot.accept();
+}
