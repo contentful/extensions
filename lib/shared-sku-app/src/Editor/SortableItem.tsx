6305f9d4f06ac111f1f6b8e496105f83a7c32111
Yiotis Kaltsikis <giotisgr@gmail.com>
2019-11-25 16:52:56 +0100

    Improve product rendering

--- lib/shared-sku-app/src/Editor/SortableItem.tsx
+++ lib/shared-sku-app/src/Editor/SortableItem.tsx
@@ -1,58 +1,74 @@
 import * as React from 'react';
 import { SortableElement, SortableHandle } from 'react-sortable-hoc';
 import { css } from 'emotion';
-import { IconButton, Card } from '@contentful/forma-36-react-components';
+import {
+  DropdownList,
+  DropdownListItem,
+  Card,
+  CardDragHandle as FormaCardDragHandle,
+  CardActions
+} from '@contentful/forma-36-react-components';
+import tokens from '@contentful/forma-36-tokens';
 
-interface DragHandleProps {
+interface SortableElementProps {
   url: string | undefined;
   alt: string | undefined;
-}
-
-interface SortableElementProps extends DragHandleProps {
   disabled: boolean;
   onDelete: () => void;
 }
 
-const DragHandle = SortableHandle<DragHandleProps>(({ url, alt }: DragHandleProps) =>
-  url ? <img src={url} alt={alt} /> : <div>Asset not available</div>
-);
-
 const styles = {
   card: (disabled: boolean) =>
     css({
-      margin: '10px',
+      display: 'flex',
+      // height: '150px',
+      padding: 0,
       position: 'relative',
-      width: '150px',
-      height: '100px',
-      '> img': {
+      '& ~ &': css({
+        marginTop: tokens.spacingXs
+      }),
+      '> img': css({
         cursor: disabled ? 'move' : 'pointer',
         display: 'block',
         maxWidth: '150px',
         maxHeight: '100px',
-        margin: 'auto',
+        margin: 'auto 0 auto auto',
+        padding: `${tokens.spacingL} ${tokens.spacingM} ${tokens.spacingM} ${tokens.spacingM}`,
         userSelect: 'none' // Image selection sometimes makes drag and drop ugly.
-      }
+      })
     }),
-  remove: css({
+  dragHandle: css({
+    height: 'auto'
+  }),
+  actions: css({
     position: 'absolute',
-    top: '-10px',
-    right: '-10px',
-    backgroundColor: 'white'
+    top: tokens.spacingXs,
+    right: tokens.spacingXs
   })
 };
+
+const CardDragHandle = SortableHandle(() => (
+  <FormaCardDragHandle className={styles.dragHandle}>Reorder product</FormaCardDragHandle>
+));
 
 export const SortableItem = SortableElement<SortableElementProps>((props: SortableElementProps) => {
   return (
     <Card className={styles.card(props.disabled)}>
-      <DragHandle url={props.url} alt={props.alt} />
+      {props.url ? (
+        <>
+          <CardDragHandle />
+          <img src={props.url} alt={props.alt} />
+        </>
+      ) : (
+        <div>Product not available</div>
+      )}
       {!props.disabled && (
-        <IconButton
-          label="Close"
-          onClick={props.onDelete}
-          className={styles.remove}
-          iconProps={{ icon: 'Close' }}
-          buttonType="muted"
-        />
+        <CardActions className={styles.actions}>
+          <DropdownList>
+            <DropdownListItem isTitle>Actions</DropdownListItem>
+            <DropdownListItem onClick={props.onDelete}>Remove</DropdownListItem>
+          </DropdownList>
+        </CardActions>
       )}
     </Card>
   );
--- lib/shared-sku-app/src/Editor/SortableList.tsx
+++ lib/shared-sku-app/src/Editor/SortableList.tsx
@@ -1,69 +1,30 @@
 import * as React from 'react';
 import { SortableContainer } from 'react-sortable-hoc';
-import { css } from 'emotion';
-import { Hash, DeleteFn } from '../interfaces';
+import { DeleteFn } from '../interfaces';
 import { SortableItem } from './SortableItem';
 
 interface SortableContainerProps {
   disabled: boolean;
-  config: Hash;
   productPreviews: string[][];
   deleteFn: DeleteFn;
 }
 
-const styles = {
-  container: css({
-    maxWidth: '600px'
-  }),
-  grid: css({
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)'
-  })
-};
-
 export const SortableList = SortableContainer<SortableContainerProps>(
-  (props: SortableContainerProps) => {
-    // Provide stable keys for all resources so images don't blink.
-    const { list } = props.productPreviews.reduce(
-      (acc: any, preview, index) => {
-        const [url, alt] = preview;
-        const item = { url, alt, key: `url-unknown-${index}` };
-        const counts = { ...acc.counts };
-
-        // URLs are used as keys.
-        // It is possible to include the same image more than once.
-        // We count usages of the same URL and use the count in keys.
-        // This can be considered an edge-case but still - should be covered.
-        if (url) {
-          counts[url] = counts[url] || 1;
-          item.key = [url, counts[url]].join('-');
-          counts[url] += 1;
-        }
-
-        return {
-          counts,
-          list: [...acc.list, item]
-        };
-      },
-      { counts: {}, list: [] }
-    ) as { list: Hash[] };
-
+  ({ disabled, deleteFn, productPreviews }: SortableContainerProps) => {
     return (
-      <div className={styles.container}>
-        <div className={styles.grid}>
-          {list.map(({ url, alt, key }, index) => {
-            return (
-              <SortableItem
-                disabled={props.disabled}
-                key={key}
-                url={url}
-                alt={alt}
-                index={index}
-                onDelete={() => props.deleteFn(index)}
-              />
-            );
-          })}
-        </div>
+      <div>
+        {productPreviews.map(([url, alt], index) => {
+          return (
+            <SortableItem
+              disabled={disabled}
+              key={`${url}-${index}`}
+              url={url}
+              alt={alt}
+              index={index}
+              onDelete={() => deleteFn(index)}
+            />
+          );
+        })}
       </div>
     );
   }
