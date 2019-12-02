7d6cfe6dcce9fba010a5b45577bb88138caad10d
Yiotis Kaltsikis <giotisgr@gmail.com>
2019-12-02 17:14:23 +0100

    Simplify paginator code

--- lib/shared-sku-app/src/SkuPicker/Paginator/index.tsx
+++ lib/shared-sku-app/src/SkuPicker/Paginator/index.tsx
@@ -13,27 +13,37 @@ export interface Props {
 }
 
 export function getPagesRange(page: number, total: number, neighboursCount = 2): number[] {
-  if (total < neighboursCount * 2 + 1) {
+  const PAGINATOR_RANGE = neighboursCount * 2;
+
+  if (total <= PAGINATOR_RANGE) {
+    // Total amount of pages are less than the possible paginator range
     return range(0, total);
   }
   if (page <= neighboursCount) {
-    return range(0, neighboursCount * 2 + 1);
+    // Active page is at the start of the paginator page count
+    return range(0, PAGINATOR_RANGE + 1);
   }
   if (page > total - neighboursCount) {
-    return range(total - neighboursCount * 2 - 1, total);
+    // Active page is at the end of the paginator page count
+    return range(total - PAGINATOR_RANGE - 1, total);
   }
+  // Active page is in the middle of the paginator count
   return range(page - neighboursCount - 1, page + neighboursCount);
 }
 
 export function Paginator(props: Props) {
   const { className, pageCount, setActivePage } = props;
   const activePage = clamp(props.activePage, 1, pageCount);
+  const hasOnlyOnePage = pageCount === 1;
+  const activePageIsAtPaginatorStart = activePage === 1;
+  const activePageIsAtPaginatorEnd = activePage === pageCount;
+
   return (
     <div className={className}>
       <Button
         className={styles.button}
         buttonType="muted"
-        disabled={pageCount === 1 || activePage === 1}
+        disabled={hasOnlyOnePage || activePageIsAtPaginatorStart}
         onClick={() => setActivePage(1)}>
         <img className={styles.chevronLeft} src={doubleChevron} alt="right" />
       </Button>
@@ -41,7 +51,7 @@ export function Paginator(props: Props) {
         icon="ChevronLeft"
         className={styles.button}
         buttonType="muted"
-        disabled={pageCount === 1 || activePage === 1}
+        disabled={hasOnlyOnePage || activePageIsAtPaginatorStart}
         onClick={() => setActivePage(activePage - 1)}
       />
       {getPagesRange(activePage, pageCount).map(pageIndex => {
@@ -61,13 +71,13 @@ export function Paginator(props: Props) {
         icon="ChevronRight"
         buttonType="muted"
         className={styles.button}
-        disabled={pageCount === 1 || activePage === pageCount}
+        disabled={hasOnlyOnePage || activePageIsAtPaginatorEnd}
         onClick={() => setActivePage(activePage + 1)}
       />
       <Button
         buttonType="muted"
         className={styles.button}
-        disabled={pageCount === 1 || activePage === pageCount}
+        disabled={hasOnlyOnePage || activePageIsAtPaginatorEnd}
         onClick={() => setActivePage(pageCount)}>
         <img className={styles.chevronRight} src={doubleChevron} alt="right" />
       </Button>
