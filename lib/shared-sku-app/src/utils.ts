327131164ed685acde6a10ac8f4a92a766ab0fe7
Yiotis Kaltsikis <giotisgr@gmail.com>
2019-12-03 11:45:42 +0100

    Refactor mapSort to prevent array mutations

--- lib/shared-sku-app/src/utils.ts
+++ lib/shared-sku-app/src/utils.ts
@@ -4,10 +4,10 @@
  * @see https://gist.github.com/ecarter/1423674
  */
 export const mapSort = (array: any[], order: any[], key: string) => {
-  array.sort((a, b) => {
+  const sorted = array.slice().sort((a, b) => {
     const A = a[key];
     const B = b[key];
     return order.indexOf(A) > order.indexOf(B) ? 1 : -1;
   });
-  return array;
+  return sorted;
 };
