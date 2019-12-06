ff35cb7b311c335039b106f2181d96b94040cf01
Yiotis Kaltsikis <giotiskl@users.noreply.github.com>
2019-12-06 10:22:11 +0100

    Enable SVG support in Image Uploader (#263)

--- samples/image-uploader/src/index.js
+++ samples/image-uploader/src/index.js
@@ -1,151 +1,145 @@
-import React from "react"
-import PropTypes from "prop-types"
-import ReactDOM from "react-dom"
-import "@contentful/forma-36-react-components/dist/styles.css"
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import '@contentful/forma-36-react-components/dist/styles.css';
 
-import { Spinner } from "@contentful/forma-36-react-components"
-import { init } from "contentful-ui-extensions-sdk"
-import UploadView from "./components/UploadView"
-import ProgressView from "./components/ProgressView"
-import FileView from "./components/FileView"
+import { Spinner } from '@contentful/forma-36-react-components';
+import { init } from 'contentful-ui-extensions-sdk';
+import UploadView from './components/UploadView';
+import ProgressView from './components/ProgressView';
+import FileView from './components/FileView';
 import {
   readFileAsUrl,
   findImageContentType,
   getImageUrlFromDataTransfer,
   getAssetIdFromDataTransfer,
   getBase64FromDataTransfer
-} from "./utils"
+} from './utils';
 
-import "./index.css"
+import './index.css';
 
 class App extends React.Component {
   static propTypes = {
     sdk: PropTypes.object.isRequired
-  }
+  };
 
   state = {
     isDraggingOver: false,
     value: this.props.sdk.field.getValue(this.findProperLocale())
-  }
+  };
 
   componentDidMount() {
-    this.props.sdk.window.startAutoResizer()
+    this.props.sdk.window.startAutoResizer();
 
     // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
-    this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(
-      this.onExternalChange
-    )
+    this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(this.onExternalChange);
 
     if (this.state.value) {
       this.props.sdk.space
         .getAsset(this.state.value.sys.id)
         .then(asset => this.setState({ asset }))
-        .catch(() => this.unlinkAsset())
+        .catch(() => this.unlinkAsset());
     }
   }
 
   componentWillUnmount() {
-    this.detachExternalChangeHandler()
+    this.detachExternalChangeHandler();
   }
 
   onDropFiles = event => {
-    event.preventDefault()
-    event.stopPropagation()
+    event.preventDefault();
+    event.stopPropagation();
 
     this.setState({
       imageUrl: undefined,
       base64Prefix: undefined,
       base64Data: undefined
-    })
+    });
 
     // Read the file that was just selected
-    const files = Array.prototype.slice.call(
-      event.target.files || event.dataTransfer.files
-    )
+    const files = Array.prototype.slice.call(event.target.files || event.dataTransfer.files);
 
     if (files.length) {
-      return this.createNewAssetFromFiles(files)
+      return this.createNewAssetFromFiles(files);
     }
 
     if (!event.dataTransfer) {
-      return
+      return;
     }
 
     // Check if another asset was dragndropped.
-    const assetId = getAssetIdFromDataTransfer(event.dataTransfer)
+    const assetId = getAssetIdFromDataTransfer(event.dataTransfer);
     if (assetId) {
-      return this.reuseExistingAsset(assetId)
+      return this.reuseExistingAsset(assetId);
     }
 
     // Check if an image with base64 type was dragndropped
-    const base64 = getBase64FromDataTransfer(event.dataTransfer)
+    const base64 = getBase64FromDataTransfer(event.dataTransfer);
     if (base64) {
       return this.createNewAssetFromBase64(base64.prefix, base64.data, {
-        name: "Unnamed",
+        name: 'Unnamed',
         type: base64.type
-      })
+      });
     }
 
     // Check if an image element was dragndropped
-    const imageUrl = getImageUrlFromDataTransfer(event.dataTransfer)
+    const imageUrl = getImageUrlFromDataTransfer(event.dataTransfer);
     if (imageUrl) {
-      return this.createNewAssetFromImageUrl(imageUrl)
+      return this.createNewAssetFromImageUrl(imageUrl);
     }
-  }
+  };
 
   onChange = event => {
-    const value = event.currentTarget.value
-    this.setState({ value })
+    const value = event.currentTarget.value;
+    this.setState({ value });
 
     if (value) {
-      this.props.sdk.field.setValue(value)
+      this.props.sdk.field.setValue(value);
     } else {
-      this.props.sdk.field.removeValue()
+      this.props.sdk.field.removeValue();
     }
-  }
+  };
 
   onClickEdit = () => {
     this.props.sdk.navigator.openAsset(this.state.asset.sys.id, {
       slideIn: true
-    })
-  }
+    });
+  };
 
   onClickLinkExisting = async () => {
     const selectedAsset = await this.props.sdk.dialogs.selectSingleAsset({
       locale: this.props.sdk.field.locale
-    })
+    });
 
     try {
-      await this.setFieldLink(selectedAsset.sys.id)
+      await this.setFieldLink(selectedAsset.sys.id);
     } catch (err) {
-      this.onError(err)
+      this.onError(err);
     }
-  }
+  };
 
   onClickRemove = () => {
-    this.unlinkAsset()
-  }
+    this.unlinkAsset();
+  };
 
   onDragOverEnd = () => {
-    this.setState({ isDraggingOver: false })
-  }
+    this.setState({ isDraggingOver: false });
+  };
 
   onDragOverStart = () => {
-    this.setState({ isDraggingOver: true })
-  }
+    this.setState({ isDraggingOver: true });
+  };
 
   onError = error => {
-    console.error(error)
-    this.props.sdk.notifier.error(error.message)
-  }
+    this.props.sdk.notifier.error(error.message);
+  };
 
   onExternalChange = value => {
-    this.setState({ value })
-  }
+    this.setState({ value });
+  };
 
   /*
      Create a new (unprocessed) asset entry for given upload and file.
-
      createAsset(upload: UploadEntity, file: File, locale: string): Promise<AssetEntity>
   */
   createAsset = (upload, file, locale) => {
@@ -155,26 +149,25 @@ class App extends React.Component {
         description: {},
         file: {}
       }
-    }
+    };
 
-    asset.fields.title[locale] = file.name
-    asset.fields.description[locale] = file.name
+    asset.fields.title[locale] = file.name;
+    asset.fields.description[locale] = file.name;
     asset.fields.file[locale] = {
       contentType: file.type,
       fileName: file.name,
       uploadFrom: {
         sys: {
-          type: "Link",
-          linkType: "Upload",
+          type: 'Link',
+          linkType: 'Upload',
           id: upload.sys.id
         }
       }
-    }
+    };
 
-    return this.props.sdk.space.createAsset(asset)
-  }
+    return this.props.sdk.space.createAsset(asset);
+  };
 
-  // createAssetWithImageUrl(imageUrl, contentType, locale)
   createAssetWithImageUrl = (imageUrl, contentType, locale) => {
     const asset = {
       fields: {
@@ -182,80 +175,79 @@ class App extends React.Component {
         description: {},
         file: {}
       }
-    }
+    };
 
-    asset.fields.title[locale] = imageUrl
-    asset.fields.description[locale] = imageUrl
+    asset.fields.title[locale] = imageUrl;
+    asset.fields.description[locale] = imageUrl;
     asset.fields.file[locale] = {
       contentType,
       fileName: imageUrl,
       upload: imageUrl
-    }
+    };
 
-    return this.props.sdk.space.createAsset(asset)
-  }
+    return this.props.sdk.space.createAsset(asset);
+  };
 
   /*
     If customers prefers localization of references, always return default locale.
     If not, return current locale.
   */
   findProperLocale() {
-    if (this.props.sdk.entry.fields[this.props.sdk.field.id].type === "Link") {
-      return this.props.sdk.locales.default
+    if (this.props.sdk.entry.fields[this.props.sdk.field.id].type === 'Link') {
+      return this.props.sdk.locales.default;
     }
 
-    return this.props.sdk.field.locale
+    return this.props.sdk.field.locale;
   }
 
-  // reuseExistingAsset(assetId)
   reuseExistingAsset = async assetId => {
-    let asset
+    let asset;
 
     try {
-      asset = await this.props.sdk.space.getAsset(assetId)
+      asset = await this.props.sdk.space.getAsset(assetId);
     } catch (err) {
-      this.onError(err)
+      this.onError(err);
     }
 
     this.setState({
       asset
-    })
+    });
 
     await this.props.sdk.field.setValue(
       {
         sys: {
-          type: "Link",
-          linkType: "Asset",
+          type: 'Link',
+          linkType: 'Asset',
           id: assetId
         }
       },
       this.findProperLocale()
-    )
-  }
+    );
+  };
 
   createNewAssetFromFiles = async files => {
     // Filter only images
-    const imageFiles = files.filter(file => /^image\/[\w-_]+$/.test(file.type))
+    const imageFiles = files.filter(file => /^image\/[\w-_|\w+\w]+$/.test(file.type));
 
     // If no images were found, raise an error
     if (imageFiles.length === 0) {
-      return this.onError(new Error("Only images are allowed"))
+      return this.onError(new Error('Only images are allowed'));
     }
 
     // Only one image at a time is supported. In the future, we can accept set of images per locale ?
     if (imageFiles.length > 1) {
-      return this.onError(new Error("Please drop only one image at a time"))
+      return this.onError(new Error('Please drop only one image at a time'));
     }
 
-    const imageFile = imageFiles[0]
+    const imageFile = imageFiles[0];
 
-    this.setState({ file: imageFile })
-    this.setUploadProgress(0)
+    this.setState({ file: imageFile });
+    this.setUploadProgress(0);
 
     // Encode the file as Base64, so we can pass it through SDK proxy to get it uploaded
-    const [base64Prefix, base64Data] = await readFileAsUrl(imageFile)
-    this.createNewAssetFromBase64(base64Prefix, base64Data, imageFile)
-  }
+    const [base64Prefix, base64Data] = await readFileAsUrl(imageFile);
+    this.createNewAssetFromBase64(base64Prefix, base64Data, imageFile);
+  };
 
   /* `createNewAssetFromFile(file?: File): void` takes base64 data
      that contains the image and performs following tasks;
@@ -267,37 +259,37 @@ class App extends React.Component {
      * Publish the asset
      */
   createNewAssetFromBase64 = async (base64Prefix, base64Data, file) => {
-    this.setUploadProgress(10)
-    this.setState({ base64Prefix, base64Data })
+    this.setUploadProgress(10);
+    this.setState({ base64Prefix, base64Data });
 
     // Upload the Base64 encoded image
-    const upload = await this.props.sdk.space.createUpload(base64Data)
-    this.setUploadProgress(40)
+    const upload = await this.props.sdk.space.createUpload(base64Data);
+    this.setUploadProgress(40);
 
     // Some customers use different locale model than others, so we need to figure out what works for them best
-    const locale = this.findProperLocale()
+    const locale = this.findProperLocale();
 
     // Create an unprocessed asset record that links to the upload record created above
     // It reads asset title and filenames from the HTML5 File object we're passing as second parameter
-    const rawAsset = await this.createAsset(upload, file, locale)
-    this.setUploadProgress(50)
-    this.processAndPublishAsset(rawAsset, locale)
-  }
+    const rawAsset = await this.createAsset(upload, file, locale);
+    this.setUploadProgress(50);
+    this.processAndPublishAsset(rawAsset, locale);
+  };
 
   createNewAssetFromImageUrl = async imageUrl => {
-    this.setUploadProgress(0)
+    this.setUploadProgress(0);
 
     this.setState({
       imageUrl
-    })
+    });
 
     // const contentType = await findImageContentType(imageUrl)
-    const locale = this.findProperLocale()
-    const rawAsset = await this.createAssetWithImageUrl(imageUrl, "", locale)
+    const locale = this.findProperLocale();
+    const rawAsset = await this.createAssetWithImageUrl(imageUrl, '', locale);
 
-    this.setUploadProgress(25)
-    this.processAndPublishAsset(rawAsset, locale)
-  }
+    this.setUploadProgress(25);
+    this.processAndPublishAsset(rawAsset, locale);
+  };
 
   /*
     - Send a request to start processing the asset
@@ -306,60 +298,60 @@ class App extends React.Component {
   */
   processAndPublishAsset = async (rawAsset, locale) => {
     // Send a request to start processing the asset. This will happen asynchronously.
-    await this.props.sdk.space.processAsset(rawAsset, locale)
+    await this.props.sdk.space.processAsset(rawAsset, locale);
 
-    this.setUploadProgress(55)
+    this.setUploadProgress(55);
 
     // Wait until asset is processed.
     const processedAsset = await this.props.sdk.space.waitUntilAssetProcessed(
       rawAsset.sys.id,
       locale
-    )
-    this.setUploadProgress(85)
+    );
+    this.setUploadProgress(85);
 
     // Publish the asset, ignore if it fails
-    let publishedAsset
+    let publishedAsset;
     try {
-      publishedAsset = await this.props.sdk.space.publishAsset(processedAsset)
+      publishedAsset = await this.props.sdk.space.publishAsset(processedAsset);
     } catch (err) {}
 
-    this.setUploadProgress(95)
+    this.setUploadProgress(95);
 
-    const asset = publishedAsset || processedAsset
+    const asset = publishedAsset || processedAsset;
     this.setState({
       asset
-    })
+    });
 
     // Set the value of the reference field as a link to the asset created above
     await this.props.sdk.field.setValue(
       {
         sys: {
-          type: "Link",
-          linkType: "Asset",
+          type: 'Link',
+          linkType: 'Asset',
           id: asset.sys.id
         }
       },
       locale
-    )
+    );
 
-    this.setUploadProgress(100)
-  }
+    this.setUploadProgress(100);
+  };
 
   unlinkAsset = () => {
-    this.props.sdk.field.setValue(null, this.findProperLocale())
+    this.props.sdk.field.setValue(null, this.findProperLocale());
     this.setState({
       value: null,
       asset: null
-    })
-  }
+    });
+  };
 
   setFieldLink(assetId) {
     return this.props.sdk.field
       .setValue(
         {
           sys: {
-            type: "Link",
-            linkType: "Asset",
+            type: 'Link',
+            linkType: 'Asset',
             id: assetId
           }
         },
@@ -369,14 +361,14 @@ class App extends React.Component {
         this.props.sdk.space
           .getAsset(this.state.value.sys.id)
           .then(asset => this.setState({ asset }))
-      )
+      );
   }
 
   setUploadProgress(percent) {
     this.setState({
       uploading: percent < 100,
       uploadProgress: percent
-    })
+    });
   }
 
   render = () => {
@@ -388,15 +380,14 @@ class App extends React.Component {
           base64Data={this.state.base64Data}
           uploadProgress={this.state.uploadProgress}
         />
-      )
+      );
     } else if (!this.state.isDraggingOver && this.state.asset) {
       // Display existing asset if user is not dragging over an image
       return (
         <FileView
           file={this.state.asset.fields.file[this.findProperLocale()]}
           isPublished={
-            this.state.asset.sys.version ===
-            (this.state.asset.sys.publishedVersion || 0) + 1
+            this.state.asset.sys.version === (this.state.asset.sys.publishedVersion || 0) + 1
           }
           isDraggingOver={this.state.isDraggingOver}
           onDrop={this.onDropFiles}
@@ -405,7 +396,7 @@ class App extends React.Component {
           onClickEdit={this.onClickEdit}
           onClickRemove={this.onClickRemove}
         />
-      )
+      );
     } else if (!this.state.isDraggingOver && this.state.value) {
       // If `asset` is not set but `value` is, the entry was just opened
       // and we're currently loading the asset value.
@@ -413,7 +404,7 @@ class App extends React.Component {
         <main className="spinner viewport centered">
           <Spinner />
         </main>
-      )
+      );
     }
 
     return (
@@ -424,14 +415,14 @@ class App extends React.Component {
         onDragOverEnd={this.onDragOverEnd}
         onClickLinkExisting={this.onClickLinkExisting}
       />
-    )
-  }
+    );
+  };
 }
 
 init(sdk => {
-  ReactDOM.render(<App sdk={sdk} />, document.getElementById("root"))
-})
+  ReactDOM.render(<App sdk={sdk} />, document.getElementById('root'));
+});
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
